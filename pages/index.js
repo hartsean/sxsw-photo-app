c92518eff0f4993946c7cd4852a389e19fb66ccf
Sean Hart <seanhart@Seans-MBP.attlocal.net>
2022-01-07 13:22:27 -0600

    (imgs added) init commit

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,29 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>sean hart</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="SXSW Photo Team Application" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Photos
         </p>
-      </main>
 
+        <img src="/imgs/IMG_1031.JPG" alt="#1"></img>
+        <img src="/imgs/IMG_1481.JPG" alt="#2"></img>
+        <img src="/imgs/IMG_1734.JPG" alt="#3"></img>
+        <img src="/imgs/IMG_1801.JPG" alt="#4"></img>
+        {/* <img src="/imgs/IMG_1816.JPG" alt="#5"></img> */}
+        <img src="/imgs/IMG_1821.JPG" alt="#6"></img>
+        {/* <img src="/imgs/IMG_1841.JPG" alt="#7"></img>
+        <img src="/imgs/IMG_1843.JPG" alt="#8"></img> */}
+        <img src="/imgs/IMG_1844.JPG" alt="#9"></img>
+        {/* <img src="/imgs/IMG_1845.JPG" alt="#10"></img> */}
+        <img src="/imgs/IMG_2561.JPG" alt="#10"></img>
+
+      </main>
       <Footer />
     </div>
   )
